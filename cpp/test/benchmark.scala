e3b50e4f3e844476107db2c583afdb134c84cf70
Kaspar Fischer (hbf) <kaspar.fischer@dreizak.com>
2013-03-24 16:24:41 +0100

    minor changes for performance plot

--- cpp/test/benchmark.scala
+++ cpp/test/benchmark.scala
@@ -12,10 +12,13 @@ def time(n: Int, dim: Int) = {
   time.toDouble
 }
 
-def avg(n: Int, dim: Int) = (1 to 10).map(_ => time(n, dim)).sum/10.0
+def avg(n: Int, dim: Int) = {
+  println(s"Measuring (d,n)=($dim,$n) ...")
+  (1 to 10).map(_ => time(n, dim)).sum/10.0
+}
 
 val plots = Map(
-    3 -> List(10000, 50000, 100000, 250000, 500000, 750000, 1000000, 2000000, 5000000),
+    3 -> List(10000, 50000, 100000, 250000, 500000, 750000, 1000000, 2000000),
     5 -> List(10000, 50000, 100000, 250000, 500000, 750000, 1000000, 2000000),
     10 -> List(10000, 50000, 100000, 250000, 500000, 750000, 1000000)
   )
@@ -30,7 +33,7 @@ val data = measures.map { case (d, times) => times.map { case (n,t) => s" $n $t\
 
 val plot = """set terminal png size 500,380
 set output 'times.png'
-set xtics ("10k" 10000, "50k" 50000, "100k" 100000, "250k" 250000, "500k" 500000, "1m" 1000000, "2m" 2000000, "5m" 5000000)
+set xtics ("10k" 10000, "100k" 100000, "250k" 250000, "500k" 500000, "1m" 1000000, "2m" 2000000, "5m" 5000000)
 set xlabel "number of points"
 set ylabel "time (s)"
 plot """ + headers + "\n" + data
