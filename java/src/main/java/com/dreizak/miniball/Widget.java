37bd58bd441a106970b8d47b6c0ecb091dc00b3d
Kaspar Fischer (hbf) <kaspar.fischer@dreizak.com>
2013-04-16 12:02:30 +0200

    some initial work on Java version
    Only added Maven project structure so far...

---
+++ java/src/main/java/com/dreizak/miniball/Widget.java
@@ -0,0 +1,36 @@
+package com.dreizak.miniball;
+
+/**
+ * Simple wigdet class that can be enabled or disabled
+ */
+public class Widget {
+
+	private boolean enabled;
+
+	/**
+	 * Create new enabled widget
+	 */
+	public Widget() {
+		enabled = true;
+	}
+
+	/**
+	 * Is the widget currently enabled?
+	 * 
+	 * @return true if enabled, false otherwise
+	 */
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	/**
+	 * Set the widget enable state
+	 * 
+	 * @param enabled
+	 * @return this widget
+	 */
+	public Widget setEnabled(boolean enabled) {
+		this.enabled = enabled;
+		return this;
+	}
+}
