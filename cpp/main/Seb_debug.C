a536a4dd61f0be32ddcdce01edbce481c38fe1c1
Kaspar Fischer (hbf) <kaspar.fischer@dreizak.com>
2013-04-17 10:24:27 +0200

    indentation
    (no changes content-wise)

--- cpp/main/Seb_debug.C
+++ cpp/main/Seb_debug.C
@@ -10,23 +10,23 @@
 #include "Seb_configure.h"
 
 namespace SEB_NAMESPACE {
-
+  
   // Implementation of class Logger:
-
+  
   Logger::Logger()
   {
   }
-
+  
   Logger::~Logger()
   {
     // we walk through the list of all opened files and close them:
     for (Streams::iterator it = channels.begin();
-	 it != channels.end(); ++it) {
+         it != channels.end(); ++it) {
       (*it).second->close();
       delete (*it).second;
     }
   }
-
+  
   Logger& Logger::instance()
   {
     // Here's where we maintain the only instance: (Notice that it
@@ -36,30 +36,30 @@ namespace SEB_NAMESPACE {
     static Logger instance;
     return instance;
   }
-
+  
   void Logger::log(const char* ch,const std::string& msg)
   {
     const std::string name(ch);
     Streams::iterator it = channels.find(name);
-
+    
     // have we already opened this file?
     if (it != channels.end()) {
       // If so, then just append the message:
       *(*it).second << msg;
-       (*it).second->flush();
-
+      (*it).second->flush();
+      
     } else {
       // If we haven't seen 'name' before, we create a new file:
       using std::ofstream;
       ofstream *o = new ofstream((name+".log").c_str(),
-				 ofstream::out|ofstream::trunc);
+                                 ofstream::out|ofstream::trunc);
       channels[name] = o;
       *o << msg;
     }
   }
-
+  
   // Implementation of class Timer:
-
+  
   // The following routine is taken from file mptimeval.h from
   // "Matpack Library Release 1.7.1" which is copyright (C) 1991-2002
   // by Berndt M. Gammel.  It works on the timeval struct defined in
@@ -79,11 +79,11 @@ namespace SEB_NAMESPACE {
     }
     return t1;
   }
-
+  
   Timer::Timer()
   {
   }
-
+  
   Timer& Timer::instance()
   {
     // Here's where we maintain the only instance: (Notice that it
@@ -93,31 +93,31 @@ namespace SEB_NAMESPACE {
     static Timer instance;
     return instance;
   }
-
+  
   void Timer::start(const char *timer_name)
   {
     // fetch current usage:
     rusage now;
     int status = getrusage(RUSAGE_SELF,&now);
     SEB_ASSERT(status == 0);
-
+    
     // save it:
     timers[std::string(timer_name)] = now.ru_utime;
   }
-
+  
   float Timer::lapse(const char *name)
   {
     // assert that start(name) has been called before:
     SEB_ASSERT(timers.find(std::string(name)) != timers.end());
-
+    
     // get current usage:
     rusage now;
     int status = getrusage(RUSAGE_SELF,&now);
     SEB_ASSERT(status == 0);
-
+    
     // compute elapsed usage:
     now.ru_utime -= (*timers.find(std::string(name))).second;
     return now.ru_utime.tv_sec + now.ru_utime.tv_usec * 1e-6;
   }
-
-} // namespace SEB_NAMESPACE
+  
+  } // namespace SEB_NAMESPACE
--- cpp/main/Subspan.C
+++ cpp/main/Subspan.C
@@ -14,7 +14,7 @@
 #define SEB_AFFINE_ORIGIN S[members[r]]
 
 namespace SEB_NAMESPACE {
-
+  
   template<typename Float>
   inline void givens(Float& c, Float& s, const Float a, const Float b)
   //  Determine the Givens coefficients (c,s) satisfying
@@ -31,7 +31,7 @@ namespace SEB_NAMESPACE {
   {
     using std::abs;
     using std::sqrt;
-
+    
     if (b == 0) {
       c = 1;
       s = 0;
@@ -45,10 +45,10 @@ namespace SEB_NAMESPACE {
       s = c * t;
     }
   }
-
+  
   template<typename Float>
   Subspan<Float>::Subspan(unsigned int dim, const std::vector<Pt>& S, int index)
-    : S(S), membership(S.size()), dim(dim), members(dim+1)
+  : S(S), membership(S.size()), dim(dim), members(dim+1)
   {
     // allocate storage for Q, R, u, and w:
     Q = new Float *[dim];
@@ -59,18 +59,18 @@ namespace SEB_NAMESPACE {
     }
     u = new Float[dim];
     w = new Float[dim];
-
+    
     // initialize Q to the identity matrix:
     for (unsigned int i=0; i<dim; ++i)
       for (unsigned int j=0; j<dim; ++j)
-	Q[i][j] = (i==j)? 1 : 0;
-
+        Q[i][j] = (i==j)? 1 : 0;
+    
     members[r = 0] = index;
     membership[index] = true;
-
+    
     SEB_LOG ("ranks",r << std::endl);
   }
-
+  
   template<typename Float>
   Subspan<Float>::~Subspan()
   {
@@ -83,140 +83,140 @@ namespace SEB_NAMESPACE {
     delete[] u;
     delete[] w;
   }
-
+  
   template<typename Float>
   void Subspan<Float>::add_point(int index) {
     SEB_ASSERT(!is_member(index));
-
+    
     // compute S[i] - origin into u:
     for (unsigned int i=0; i<dim; ++i)
       u[i] = S[index][i] - SEB_AFFINE_ORIGIN[i];
-
+    
     // appends new column u to R and updates QR-decomposition,
     // routine work with old r:
     append_column();
-
+    
     // move origin index and insert new index:
     membership[index] = true;
     members[r+1] = members[r];
     members[r]   = index;
     ++r;
-
+    
     SEB_LOG ("ranks",r << std::endl);
   }
-
+  
   template<typename Float>
   void Subspan<Float>::remove_point(const unsigned int local_index) {
     SEB_ASSERT(is_member(global_index(local_index)) && size() > 1);
-
+    
     membership[global_index(local_index)] = false;
-
+    
     if (local_index == r) {
       // origin must be deleted
-
+      
       // We choose the right-most member of Q, i.e., column r-1 of R,
       // as the new origin.  So all relative vectors (i.e., the
       // columns of "A = QR") have to be updated by u:= old origin -
       // S[global_index(r-1)]:
       for (unsigned int i=0; i<dim; ++i)
-	u[i] = SEB_AFFINE_ORIGIN[i] - S[global_index(r-1)][i];
-
+        u[i] = SEB_AFFINE_ORIGIN[i] - S[global_index(r-1)][i];
+      
       --r;
-
+      
       SEB_LOG ("ranks",r << std::endl);
-
+      
       special_rank_1_update();
-
+      
     } else {
       // general case: delete column from R
-
+      
       //  shift higher columns of R one step to the left
       Float *dummy = R[local_index];
       for (unsigned int j = local_index+1; j < r; ++j) {
-	R[j-1] = R[j];
-	members[j-1] = members[j];
+        R[j-1] = R[j];
+        members[j-1] = members[j];
       }
       members[r-1] = members[r];  // shift down origin
       R[--r] = dummy;             // relink trash column
-
+      
       // zero out subdiagonal entries in R
       hessenberg_clear(local_index);
     }
   }
-
+  
   template<typename Float>
   template<typename RandomAccessIterator1,
-	   typename RandomAccessIterator2>
+  typename RandomAccessIterator2>
   Float Subspan<Float>::
   shortest_vector_to_span(RandomAccessIterator1 p,
-			  RandomAccessIterator2 w)
+                          RandomAccessIterator2 w)
   {
     using std::inner_product;
-
+    
     // compute vector from p to origin, i.e., w = origin - p:
     for (unsigned int i=0; i<dim; ++i)
       w[i] = SEB_AFFINE_ORIGIN[i] - p[i];
-
+    
     // remove projections of w onto the affine hull:
     for (unsigned int j = 0; j < r; ++j) {
       const Float scale = inner_product(w,w+dim,Q[j],Float(0));
       for (unsigned int i = 0; i < dim; ++i)
-	w[i] -= scale * Q[j][i];
+        w[i] -= scale * Q[j][i];
     }
-
+    
     return inner_product(w,w+dim,w,Float(0));
   }
-
+  
   template<typename Float>
   Float Subspan<Float>::representation_error()
   {
     using std::abs;
-
+    
     std::vector<Float> lambdas(size());
     Float max = 0;
     Float error;
-
+    
     // cycle through all points in hull
     for (unsigned int j = 0; j < size(); ++j) {
       // compute the affine representation:
       find_affine_coefficients(S[global_index(j)],lambdas.begin());
-
+      
       // compare coefficient of point #j to 1.0
       error = abs(lambdas[j] - 1.0);
       if (error > max) max = error;
-
+      
       // compare the other coefficients against 0.0
       for (unsigned int i = 0; i < j; ++i) {
-	error = abs(lambdas[i] - 0.0);
-	if (error > max) max = error;
+        error = abs(lambdas[i] - 0.0);
+        if (error > max) max = error;
       }
       for (unsigned int i = j+1; i < size(); ++i) {
-	error = abs(lambdas[i] - 0.0);
-	if (error > max) max = error;
+        error = abs(lambdas[i] - 0.0);
+        if (error > max) max = error;
       }
     }
-
+    
     return max;
   }
-
+  
   template<typename Float>
   template<typename RandomAccessIterator1,
-	   typename RandomAccessIterator2>
+  typename RandomAccessIterator2>
   void Subspan<Float>::
   find_affine_coefficients(RandomAccessIterator1 p,
-			   RandomAccessIterator2 lambdas)
+                           RandomAccessIterator2 lambdas)
   {
     // compute relative position of p, i.e., u = p - origin:
     for (unsigned int i=0; i<dim; ++i)
       u[i] = p[i] - SEB_AFFINE_ORIGIN[i];
-
+    
     // calculate Q^T u into w:
     for (unsigned int i = 0; i < dim; ++i) {
       w[i] = 0;
       for (unsigned int k = 0; k < dim; ++k)
-	w[i] += Q[i][k] * u[k];
+        w[i] += Q[i][k] * u[k];
     }
-
+    
     // We compute the coefficients by backsubstitution.  Notice that
     //
     //     c = \sum_{i\in M} \lambda_i (S[i] - origin)
@@ -227,13 +227,13 @@ namespace SEB_NAMESPACE {
     Float origin_lambda = 1;
     for (int j = r-1; j>=0; --j) {
       for (int k=j+1; k<r; ++k)
-	w[j] -= *(lambdas+k) * R[k][j];
+        w[j] -= *(lambdas+k) * R[k][j];
       origin_lambda -= *(lambdas+j) = w[j] / R[j][j];
     }
     // The r-th coefficient corresponds to the origin (cf. remove_point()):
     *(lambdas+r) = origin_lambda;
   }
-
+  
   template<typename Float>
   void Subspan<Float>::append_column()
   // Appends the new column u (which is a member of this instance) to
@@ -244,36 +244,36 @@ namespace SEB_NAMESPACE {
   // Precondition: r<dim
   {
     SEB_ASSERT(r < dim);
-
+    
     //  compute new column R[r] = Q^T * u
     for (unsigned int i = 0; i < dim; ++i) {
       R[r][i] = 0;
       for (unsigned int k = 0; k < dim; ++k)
-	R[r][i] += Q[i][k] * u[k];
+        R[r][i] += Q[i][k] * u[k];
     }
-
+    
     //  zero all entries R[r][dim-1] down to R[r][r+1]
     for (unsigned int j = dim-1; j > r; --j) {
       //  j is the index of the entry to be cleared
       //  with the help of entry j-1
-
+      
       //  compute Givens coefficients c,s
       Float c, s;
       givens (c,s,R[r][j-1],R[r][j]);
-
+      
       //  rotate one R-entry (the other one is an implicit zero)
       R[r][j-1] = c * R[r][j-1] + s * R[r][j];
-
+      
       //  rotate two Q-columns
       for (unsigned int i = 0; i < dim; ++i) {
-	const Float a = Q[j-1][i];
-	const Float b = Q[j][i];
-	Q[j-1][i] =  c * a + s * b;
-	Q[j][i]   =  c * b - s * a;
+        const Float a = Q[j-1][i];
+        const Float b = Q[j][i];
+        Q[j-1][i] =  c * a + s * b;
+        Q[j][i]   =  c * b - s * a;
       }
     }
   }
-
+  
   template<typename Float>
   void Subspan<Float>::hessenberg_clear (unsigned int pos)
   // Given R in lower Hessenberg form with subdiagonal entries 0 to
@@ -283,32 +283,32 @@ namespace SEB_NAMESPACE {
     //  clear new subdiagonal entries
     for (; pos < r; ++pos) {
       //  pos is the column index of the entry to be cleared
-
+      
       //  compute Givens coefficients c,s
       Float c, s;
       givens (c,s,R[pos][pos],R[pos][pos+1]);
-
+      
       //  rotate partial R-rows (of the first pair, only one entry is
       //  needed, the other one is an implicit zero)
       R[pos][pos] = c * R[pos][pos] + s * R[pos][pos+1];
       //  (then begin at posumn pos+1)
       for (unsigned int j = pos+1; j < r; ++j) {
-	const Float a = R[j][pos];
-	const Float b = R[j][pos+1];
-	R[j][pos]   =  c * a + s * b;
-	R[j][pos+1] =  c * b - s * a;
+        const Float a = R[j][pos];
+        const Float b = R[j][pos+1];
+        R[j][pos]   =  c * a + s * b;
+        R[j][pos+1] =  c * b - s * a;
       }
-
+      
       //  rotate Q-columns
       for (unsigned int i = 0; i < dim; ++i) {
-	const Float a = Q[pos][i];
-	const Float b = Q[pos+1][i];
-	Q[pos][i]   =  c * a + s * b;
-	Q[pos+1][i] =  c * b - s * a;
+        const Float a = Q[pos][i];
+        const Float b = Q[pos+1][i];
+        Q[pos][i]   =  c * a + s * b;
+        Q[pos+1][i] =  c * b - s * a;
       }
     }
   }
-
+  
   template<typename Float>
   void Subspan<Float>::special_rank_1_update ()
   // Update current QR-decomposition "A = QR" to
@@ -318,53 +318,53 @@ namespace SEB_NAMESPACE {
     for (unsigned int i = 0; i < dim; ++i) {
       w[i] = 0;
       for (unsigned int k = 0; k < dim; ++k)
-	w[i] += Q[i][k] * u[k];
+        w[i] += Q[i][k] * u[k];
     }
-
+    
     //  rotate w down to a multiple of the first unit vector;
     //  the operations have to be recorded in R and Q
     for (unsigned int k = dim-1; k > 0; --k) {
       //  k is the index of the entry to be cleared
       //  with the help of entry k-1
-
+      
       //  compute Givens coefficients c,s
       Float c, s;
       givens (c,s,w[k-1],w[k]);
-
+      
       //  rotate w-entry
       w[k-1] = c * w[k-1] + s * w[k];
-
+      
       //  rotate two R-rows;
       //  the first column has to be treated separately
       //  in order to account for the implicit zero in R[k-1][k]
       R[k-1][k]    = -s * R[k-1][k-1];
       R[k-1][k-1] *=  c;
       for (unsigned int j = k; j < r; ++j) {
-	const Float a = R[j][k-1];
-	const Float b = R[j][k];
-	R[j][k-1] =  c * a + s * b;
-	R[j][k]   =  c * b - s * a;
+        const Float a = R[j][k-1];
+        const Float b = R[j][k];
+        R[j][k-1] =  c * a + s * b;
+        R[j][k]   =  c * b - s * a;
       }
-
+      
       //  rotate two Q-columns
       for (unsigned int i = 0; i < dim; ++i) {
-	const Float a = Q[k-1][i];
-	const Float b = Q[k][i];
-	Q[k-1][i] =  c * a + s * b;
-	Q[k][i]   =  c * b - s * a;
+        const Float a = Q[k-1][i];
+        const Float b = Q[k][i];
+        Q[k-1][i] =  c * a + s * b;
+        Q[k][i]   =  c * b - s * a;
       }
     }
-
+    
     //  add w * (1,...,1)^T to new R
     //  which means simply to add u[0] to each column
     //  since the other entries of u have just been eliminated
     for (unsigned int j = 0; j < r; ++j)
       R[j][0] += w[0];
-
+    
     //  clear subdiagonal entries
     hessenberg_clear(0);
   }
-
+  
 } // namespace SEB_NAMESPACE
 
 #endif // SEB_SUBSPAN_C
